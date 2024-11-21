b2fc4ccc1470e16e44eef33b59ae2cfc349cedf2
Tshindimpho <mudzu13@gmail.com>
2024-11-21 14:26:44 +0200

    Implement the Repository

---
+++ MinimalAPIsMovies/Repositories/GenresRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIsMovies.Data;
+using MinimalAPIsMovies.Entites;
+using MinimalAPIsMovies.Repositories.Interface;
+
+namespace MinimalAPIsMovies.Repositories
+{
+    public class GenresRepository : IGenresRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenresRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Create(Genre genre)
+        {
+            // Add the genre to the database 
+            var createdGenre = _context.Genres.Add(genre);
+
+            // Save changes to the database
+            await _context.SaveChangesAsync();
+
+            // Return the ID of the newly created Genre
+            return createdGenre.Entity.Id;
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            return await _context.Genres.AnyAsync(g => g.Id == id); 
+        }
+
+        public async Task<List<Genre>> GetAll() 
+        {
+            return await _context.Genres.OrderBy(g => g.Name).ToListAsync();
+        }
+
+        public  async Task<Genre?> GetById(int id)
+        {
+            return await _context.Genres.FirstOrDefaultAsync(g => g.Id==id);
+        }
+
+        public async Task Update(Genre genre)
+        {
+            _context.Update(genre);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ MinimalAPIsMovies/Repositories/Interface/IGenresRepository.cs
@@ -0,0 +1,13 @@
+﻿using MinimalAPIsMovies.Entites;
+
+namespace MinimalAPIsMovies.Repositories.Interface
+{
+    public interface IGenresRepository
+    {
+        Task<int> Create (Genre genre);
+        Task<List<Genre>> GetAll();
+        Task<Genre?> GetById (int id);
+        Task<bool> Exists (int id);
+        Task Update (Genre genre);
+    }
+}
