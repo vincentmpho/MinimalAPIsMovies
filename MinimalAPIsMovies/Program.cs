b784e48ad7c8a98d0793cd112c429491628930fb
Tshindimpho <mudzu13@gmail.com>
2024-11-21 15:05:07 +0200

    implement CRUD operations for genres

--- MinimalAPIsMovies/Program.cs
+++ MinimalAPIsMovies/Program.cs
@@ -1,12 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPIsMovies.Data;
+using MinimalAPIsMovies.Entites;
+using MinimalAPIsMovies.Repositories;
+using MinimalAPIsMovies.Repositories.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//Service Zone- BEGIN
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//congif repository
+builder.Services.AddScoped<IGenresRepository, GenresRepository>();
+
 
 
 builder.Services.AddControllers();
@@ -14,7 +23,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+//Service Zone- END
+
 var app = builder.Build();
+
+
+//Middlewares Zone- BEGIN
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,10 +38,57 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.MapGet("/genres", async (IGenresRepository repository) =>
+{
+    return await repository.GetAll();
 
-app.UseAuthorization();
+}
+);
 
-app.MapControllers();
+app.MapGet("/genres/{id:int}", async (int id, IGenresRepository repository) =>
+{
+    var genres = await repository.GetById(id);
+
+    // Check if the genre is not found
+    if (genres is null)
+    {
+        return Results.NotFound($"Genre with ID {id} was not found in the database.");
+    }
+
+    return Results.Ok(genres);
+});
+
+
+app.MapPost("/genres", async (Genre genre, IGenresRepository repository) =>
+{
+    var id = await repository.Create(genre);
+    return Results.Created($"/genres/{id}", genre);
+});
+
+app.MapPut("/genres/{id:int}", async (int id, Genre genre, IGenresRepository repository) =>
+{
+    // Check if the genre with the given ID exists
+    var exists = await repository.Exists(id);
+
+    if (!exists)
+    {
+    
+        return Results.NotFound($"Genre with ID {id} does not exist in the database. Update failed.");
+    }
+
+    await repository.Update(genre);
+
+    return Results.Ok($"Genre with ID {id} was successfully updated.");
+});
+
+
+
+//Middlewares Zone-END
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
 
 app.Run();
