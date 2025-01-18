a3567fb7e3ce85c81398015a1ffecde22ce40399
Tshindimpho <mudzu13@gmail.com>
2025-01-18 19:28:13 +0200

    refactor: move GenreEndpoints to a separate folder for better organizatio

---
+++ MinimalAPIsMovies/Endpoints/GenresEndpoints.cs
@@ -0,0 +1,69 @@
+﻿using MinimalAPIsMovies.Entites;
+using MinimalAPIsMovies.Repositories.Interface;
+
+namespace MinimalAPIsMovies.Endpoints
+{
+    public static  class GenresEndpoints
+    {
+        public static RouteGroupBuilder MapGenres (this RouteGroupBuilder group)
+        {
+            group.MapGet("/", async (IGenresRepository repository) =>
+            {
+                return await repository.GetAll();
+
+            }
+);
+
+            group.MapGet("/{id:int}", async (int id, IGenresRepository repository) =>
+            {
+                var genres = await repository.GetById(id);
+
+                // Check if the genre is not found
+                if (genres is null)
+                {
+                    return Results.NotFound($"Genre with ID {id} was not found in the database.");
+                }
+
+                return Results.Ok(genres);
+            });
+
+            group.MapPost("/", async (Genre genre, IGenresRepository repository) =>
+            {
+                var id = await repository.Create(genre);
+                return Results.Created($"/genres/{id}", genre);
+            });
+
+            group.MapPut("/{id:int}", async (int id, Genre genre, IGenresRepository repository) =>
+            {
+                // Check if the genre with the given ID exists
+                var exists = await repository.Exists(id);
+
+                if (!exists)
+                {
+
+                    return Results.NotFound($"Genre with ID {id} does not exist in the database. Update failed.");
+                }
+
+                await repository.Update(genre);
+
+                return Results.Ok($"Genre with ID {id} was successfully updated.");
+            });
+
+            group.MapDelete("/{id:int}", async (int id, IGenresRepository repository) =>
+            {
+                // Check if the genre with the given ID exists
+                var exists = await repository.Exists(id);
+
+                if (!exists)
+                {
+                    return Results.NotFound($"Genre with ID {id} does not exist in the database. Delete failed.");
+                }
+
+                await repository.Delete(id);
+                return Results.Ok("Successfully deleted.");
+            });
+
+            return group;
+        }
+    }
+}
--- MinimalAPIsMovies/Program.cs
+++ MinimalAPIsMovies/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPIsMovies.Data;
+using MinimalAPIsMovies.Endpoints;
 using MinimalAPIsMovies.Entites;
 using MinimalAPIsMovies.Repositories;
 using MinimalAPIsMovies.Repositories.Interface;
@@ -38,66 +39,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var genresEndpoint = app.MapGroup("/genres");
-
-genresEndpoint.MapGet("/", async (IGenresRepository repository) =>
-{
-    return await repository.GetAll();
-
-}
-);
-
-genresEndpoint.MapGet("/{id:int}", async (int id, IGenresRepository repository) =>
-{
-    var genres = await repository.GetById(id);
-
-    // Check if the genre is not found
-    if (genres is null)
-    {
-        return Results.NotFound($"Genre with ID {id} was not found in the database.");
-    }
-
-    return Results.Ok(genres);
-});
-
-
-genresEndpoint.MapPost("/", async (Genre genre, IGenresRepository repository) =>
-{
-    var id = await repository.Create(genre);
-    return Results.Created($"/genres/{id}", genre);
-});
-
-genresEndpoint.MapPut("/{id:int}", async (int id, Genre genre, IGenresRepository repository) =>
-{
-    // Check if the genre with the given ID exists
-    var exists = await repository.Exists(id);
-
-    if (!exists)
-    {
-    
-        return Results.NotFound($"Genre with ID {id} does not exist in the database. Update failed.");
-    }
-
-    await repository.Update(genre);
-
-    return Results.Ok($"Genre with ID {id} was successfully updated.");
-});
-
-genresEndpoint.MapDelete("/{id:int}", async (int id, IGenresRepository repository) =>
-{
-    // Check if the genre with the given ID exists
-    var exists = await repository.Exists(id);
-
-    if (!exists)
-    {
-        return Results.NotFound($"Genre with ID {id} does not exist in the database. Delete failed.");
-    }
-
-    await repository.Delete(id);
-    return Results.Ok("Successfully deleted.");
-});
-
-
+app.MapGroup("/genres").MapGenres();
 
 //Middlewares Zone-END
 
